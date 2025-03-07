837b1c99eb89dd65e1f83de42c11f1a493e5cd39
Jia qi Hou (Beyondsoft) <v-jiaqihou@microsoft.com>
2025-03-07 11:53:03 +0800

    delete useless file

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, TestBuilder, owner } from '@trident/e2e-common';
 
-import { HomePage } from '../pages/home.page';
+import { HomePage } from '../src/pages/home.page';
 // integrate the HomePage to current test context
 const { test } = TestBuilder.create().p('home', HomePage);
 
@@ -17,17 +17,5 @@ test.describe('recommendation', () => {
         // recommendation should be visible in home page
         await expect(home.recommendation.root).toBeVisible();
 
-    });
-
-    test('should learning materials be visible @Official', async ({ home }) => {
-        owner('ktan');
-        
-        // get all the learning material titles
-        const learningMaterials =
-            await home.recommendation.getLearningMaterialTitles();
-        // check learning material cards have more than 1 items
-        expect(learningMaterials.length).toBeGreaterThanOrEqual(2);
-        // check one of the learning material titles is 'learning material 2'
-        expect(learningMaterials).toContain('learning material 2');
     });
 });
